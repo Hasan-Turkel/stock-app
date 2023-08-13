866ac55f7c7c384cd1daff3943828086cea69425
hasan <mmhotmail>
2023-08-13 22:41:23 +0300

    firm commit

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,55 +1,58 @@
 import { useEffect } from "react"
 import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
-import axios from "axios"
+import Grid from "@mui/material/Grid"
 import { useSelector } from "react-redux"
-import { fetchFail, fetchStart, getFirmsSuccess } from "../features/stockSlice"
-import { useDispatch } from "react-redux"
+import useStockCall from "../hooks/useStockCall"
+import FirmCard from "../components/FirmCard"
+// import axios from "axios"
+// import { useDispatch } from "react-redux"
+// import { fetchFail, fetchStart, getFirmsSuccess } from "../features/stockSlice"
 
 const Firms = () => {
-  const { token } = useSelector((state) => state.auth)
-  const dispatch = useDispatch()
+  // const { token } = useSelector((state) => state.auth)
+  // const dispatch = useDispatch()
 
-  const getFirms = async () => {
-    dispatch(fetchStart())
-    try {
-      const { data } = await axios(
-        `${import.meta.env.VITE_BASE_URL}/stock/firms/`,
-        {
-          headers: { Authorization: `Token ${token}` },
-        }
-      )
-      dispatch(getFirmsSuccess(data))
-      console.log(data)
-    } catch (error) {
-      dispatch(fetchFail())
-      console.log(error)
-    }
-  }
+  // const getFirms = async () => {
+  //   dispatch(fetchStart())
+  //   try {
+  //     const { data } = await axios(
+  //       `${import.meta.env.VITE_BASE_URL}/stock/firms/`,
+  //       {
+  //         headers: { Authorization: `Token ${token}` },
+  //       }
+  //     )
+  //     dispatch(getFirmsSuccess(data))
+  //     console.log(data)
+  //   } catch (error) {
+  //     dispatch(fetchFail())
+  //     console.log(error)
+  //   }
+  // }
 
-  const getSales = async () => {
-    try {
-      const { data } = await axios(
-        `${import.meta.env.VITE_BASE_URL}/stock/sales/`,
-        {
-          headers: { Authorization: `Token ${token}` },
-        }
-      )
-      console.log(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const { getStockData } = useStockCall()
+  const { firms } = useSelector((state) => state.stock)
+
   useEffect(() => {
-    getFirms()
+    // getFirms()
+    getStockData("firms")
   }, [])
 
+  console.log(firms)
   return (
     <div>
       <Typography variant="h4" color={"error"} mb={3}>
         Firms
       </Typography>
       <Button variant="contained">NEW FIRM</Button>
+
+      <Grid container justifyContent={"center"} spacing={2}>
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
