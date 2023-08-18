67671b62a38919bd35668a96b0b9dfdc82df9552
hasan <mmhotmail>
2023-08-18 16:18:52 +0300

    home commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
-
+import { Typography } from "@mui/material"
+import KpiCards from "../components/KpiCards"
+import Charts from "../components/Charts"
+import useStockCall from "../hooks/useStockCall"
+import { useEffect } from "react"
 
 const Home = () => {
+  const { getStockData } = useStockCall()
+  useEffect(() => {
+    getStockData("sales")
+    getStockData("purchases")
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      <Typography variant="h4" color="error" mb={2}>
+        Dashboard
+      </Typography>
+
+      <KpiCards />
+
+      <Charts />
+    </div>
   )
 }
 
